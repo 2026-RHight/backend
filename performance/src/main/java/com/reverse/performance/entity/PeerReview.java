8badae72d71cf367a647e04b89f264409b2408aa
dmxzc <ysj7240@gmail.com>
2026-02-26 15:17:33 +0900

    [feat] 동료평가 테이블 생성(#9)

---
+++ performance/src/main/java/com/reverse/performance/entity/PeerReview.java
@@ -0,0 +1,49 @@
+package com.reverse.performance.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PeerReview {
+
+    @Id
+    @Column(name = "perr_review_id")
+    private Long id;
+
+    @Column(name = "eval_id")
+    private Long evalId;
+
+    @Column(name = "reviewer_id")
+    private Long reviewerId;
+
+    @Column(name = "communication_score")
+    private Integer communicationScore;
+
+    @Column(name = "solving_score")
+    private Integer solvingScore;
+
+    @Column(name = "responsibility_score")
+    private Integer responsibilityScore;
+
+    @Column(name = "team_contribution")
+    private Integer teamContribution;
+
+    @Column(name = "comment")
+    private String comment;
+
+    @Column(name = "eval_year")
+    private Integer evalYear;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+}
