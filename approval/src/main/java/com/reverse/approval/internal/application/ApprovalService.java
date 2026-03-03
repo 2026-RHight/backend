5a163867d6fe8393128939c1f1d936aa3735142b
Gyuwon-Jung <licmm0520@gmail.com>
2026-03-04 05:39:41 +0900

    [feat] controller와 service 임시로 만들어놓은 것 push

---
+++ approval/src/main/java/com/reverse/approval/internal/application/ApprovalService.java
@@ -0,0 +1,11 @@
+package com.reverse.approval.internal.application;
+
+import com.reverse.approval.ApprovalFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ApprovalService implements ApprovalFacade {
+
+}
---
+++ approval/src/main/java/com/reverse/approval/internal/web/ApprovalController.java
@@ -0,0 +1,47 @@
+package com.reverse.approval.internal.web;
+
+import com.reverse.approval.internal.application.ApprovalService;
+import com.reverse.approval.internal.dto.request.DraftApproval;
+import com.reverse.core.response.ApiResponse;
+import com.reverse.core.security.CustomUser;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/approval")
+public class ApprovalController implements ApprovalResource {
+
+    private final ApprovalService approvalService;
+
+    @Override
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @SecurityRequirement(name="JWT")
+    public ResponseEntity<ApiResponse<String>> draftApproval(@RequestPart(value = "dto") DraftApproval dto,
+                                                             @RequestPart(value = "files", required = false)List<MultipartFile> files,
+                                                             @AuthenticationPrincipal CustomUser user){
+        return(ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success()));
+    }
+
+    @Override
+    @PostMapping(path = "/temp",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @SecurityRequirement(name="JWT")
+    public ResponseEntity<ApiResponse<String>> tempApproval(@RequestPart(value = "dto") DraftApproval dto,
+                                                             @RequestPart(value = "files", required = false)List<MultipartFile> files,
+                                                             @AuthenticationPrincipal CustomUser user){
+        return(ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success()));
+    }
+
+
+}
