1235039d31997c5aaaa61de943ca6e980b46b57d
Ongsaem0 <tkdwnsghqkr1@gmail.com>
2026-03-04 15:01:12 +0900

    [feat]급여 파일 구성(#22)

---
+++ payroll/src/main/java/com/reverse/payroll/PayrollFacade.java
@@ -0,0 +1,4 @@
+package com.reverse.payroll;
+
+public interface PayrollFacade {
+}
---
+++ payroll/src/main/java/com/reverse/payroll/internal/domain/PayrollLedger.java
@@ -0,0 +1,7 @@
+package com.reverse.payroll.internal.domain;
+
+import lombok.Getter;
+
+@Getter
+public class PayrollLedger {
+}
