29391c2ffe05bbfe435f5649fe35d96aad79f9b3
Gyuwon-Jung <licmm0520@gmail.com>
2026-03-16 12:31:23 +0900

    [feat] local과 dev,prod일때의 S3 환경 변경 (#108)

--- core/src/main/java/com/reverse/core/config/LocalS3Config.java
+++ core/src/main/java/com/reverse/core/config/LocalS3Config.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
@@ -12,7 +13,8 @@ import software.amazon.awssdk.services.s3.S3Configuration;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
-public class S3Config {
+@Profile("local")
+public class LocalS3Config {
 
     @Value("${cloud.s3.endpoint}")
     private String endpoint;
